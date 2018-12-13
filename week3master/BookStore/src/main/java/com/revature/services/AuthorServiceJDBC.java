f8a4909f25d836a91891334e2f61aa37a197bfdb
Richard Orr <richard.orr@revature.com>
2018-12-13 10:19:56 -0500

    Prepared BookStore for adding controller layer.

---
+++ week3master/BookStore/src/main/java/com/revature/services/AuthorServiceJDBC.java
@@ -0,0 +1,38 @@
+package com.revature.services;
+
+import java.util.Set;
+
+import com.revature.beans.Author;
+import com.revature.data.AuthorDAO;
+import com.revature.data.BookAppDAOFactory;
+
+public class AuthorServiceJDBC implements AuthorService {
+	private BookAppDAOFactory bf = BookAppDAOFactory.getInstance();
+	private AuthorDAO ad = bf.getAuthorDAO();
+	
+	@Override
+	public Set<Author> getAuthors() {
+		return ad.getAuthors();
+	}
+
+	@Override
+	public Author getAuthorById(int i) {
+		return ad.getAuthor(i);
+	}
+
+	@Override
+	public void updateAuthor(Author a) {
+		ad.updateAuthor(a);
+	}
+
+	@Override
+	public void deleteAuthor(Author a) {
+		ad.deleteAuthor(a);
+	}
+
+	@Override
+	public void addAuthor(Author a) {
+		ad.addAuthor(a);
+	}
+
+}
