cd53ef8be62e6711187ca9f5feecf108118b885b
Richard Orr <richard.orr@revature.com>
2018-12-17 14:58:04 -0500

    FizzBuzz

---
+++ week4master/FizzBuzz/js/fizzbuzz.js
@@ -0,0 +1,11 @@
+// first number
+// second number
+
+// when the button is pressed, it will display fizz buzz
+// from first to second
+
+// fizz buzz is the sequence numbers printed where any
+// number divisible by 3 is replaced with 'fizz', and
+// any number divisible by 5 is replaced with 'buzz'.
+// If a number (15) is divisible by both, it will print
+// 'fizzbuzz' instead.
