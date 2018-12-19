636d7341416ae0c71e6c305755b2af1f1e6fd158
Richard Orr <richard.orr@revature.com>
2018-12-19 16:03:32 -0500

    bank stuff

--- week4master/TypeScript/Bank/main.js
+++ week4master/TypeScript/Bank/main.js
@@ -1,25 +1,25 @@
-// import { User } from './user';
-// import { Account } from './account';
+"use strict";
+exports.__esModule = true;
+var user_1 = require("./user");
+var account_1 = require("./account");
 var readline = require('readline');
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-// var users = [
-//     new User('rorr', 'pass'),
-//     new User('jamarius', 'pass'),
-//     new User('seth', 'notpass'),
-//     new User('steven', '1234'),
-//     new User('collin', 'soul'),
-//     new User('joseph', 'design pattern')
-// ];
-// users.forEach((user)=>{
-//     user.accounts = [];
-//     user.accounts.push(new Account(
-//         Math.random()*1001, 'Checking'));
-//     user.accounts.push(new Account(
-//         Math.random()*100001, 'Savings'));
-// });
+var users = [
+    new user_1.User('rorr', 'pass'),
+    new user_1.User('jamarius', 'pass'),
+    new user_1.User('seth', 'notpass'),
+    new user_1.User('steven', '1234'),
+    new user_1.User('collin', 'soul'),
+    new user_1.User('joseph', 'design pattern')
+];
+users.forEach(function (user) {
+    user.accounts = [];
+    user.accounts.push(new account_1.Account(Math.random() * 1001, 'Checking'));
+    user.accounts.push(new account_1.Account(Math.random() * 100001, 'Savings'));
+});
 var username;
 var password;
 var loggedUser;
@@ -27,28 +27,95 @@ var account;
 main();
 function main() {
     printUsers();
+    getUsername();
 }
 function printUsers() {
-    // users.forEach((user) => {console.log(user);});
-    // let maxMoney = 0;
-    // let maxUser;
-    // users.forEach((user) => {
-    //     let money = 0;
-    //     //console.log(user);
-    //     user.accounts.forEach((acct) => {money+=acct.balance});
-    //     if (maxMoney < money){
-    //         maxMoney=money;
-    //         maxUser = user.username;
-    //     }
-    // });
-    // console.log(`The user with the most money is ${maxUser} with ${maxMoney}`);
-    rl.question("Input a thing: ", function (input) {
-        console.log("You inputted: " + input);
-        next();
+    users.forEach(function (user) { console.log(user); });
+    var maxMoney = 0;
+    var maxUser;
+    users.forEach(function (user) {
+        var money = 0;
+        //console.log(user);
+        user.accounts.forEach(function (acct) { money += acct.balance; });
+        if (maxMoney < money) {
+            maxMoney = money;
+            maxUser = user.username;
+        }
     });
-    function next() {
-        rl.question("Input another thing: ", function (input) {
-            console.log("Also: " + input);
+    console.log("The user with the most money is " + maxUser + " with " + maxMoney);
+}
+function getUsername() {
+    rl.question("Input your username: ('q' to quit) ", function (answer) {
+        if (answer === 'q') {
+            exit();
+        }
+        else {
+            username = answer;
+            getPassword();
+        }
+    });
+}
+function getPassword() {
+    rl.question("Input your password: ", function (answer) {
+        password = answer;
+        login();
+    });
+}
+function login() {
+    loggedUser = users.find(function (user) { return user.login(username, password); });
+    if (loggedUser) {
+        options();
+    }
+    else {
+        getUsername();
+    }
+}
+function options() {
+    loggedUser.accounts.forEach(function (acc) {
+        console.log(acc.type);
+    });
+    rl.question("Choose an account: ('q' to quit) ", function (answer) {
+        if (answer === 'q') {
+            exit();
+        }
+        else {
+            account = loggedUser.accounts.find(function (acc) { return acc.type === answer; });
+            if (account) {
+                operations();
+            }
+            else {
+                options();
+            }
+        }
+    });
+    function operations() {
+        console.log("Current Balance: " + account.balance);
+        rl.question("'w' for withdraw, 'd' for deposit: ", function (answer) {
+            if (answer === 'w') {
+                rl.question("Amount: ", function (answer) {
+                    account.withdraw(+answer);
+                    console.log("Current Balance: " + account.balance);
+                    console.log("Logging out");
+                    getUsername();
+                });
+            }
+            else {
+                if (answer === 'd') {
+                    rl.question("Amount: ", function (answer) {
+                        account.deposit(+answer);
+                        console.log("Current Balance: " + account.balance);
+                        console.log("Logging out");
+                        getUsername();
+                    });
+                }
+                else {
+                    operations();
+                }
+            }
         });
     }
+}
+function exit() {
+    rl.close();
+    process.exit();
 }
